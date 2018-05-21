0a38ad22794dbefd976824ab735e6d4158096762
XKalden <kaldend.tsering@outlook.com>
2018-05-21 17:16:47 -0400

    Added pagination to User, Article

--- Gemfile
+++ Gemfile
@@ -10,6 +10,10 @@ gem 'rails', '~> 5.2.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 
+# gems to add pagination
+gem 'will_paginate', '3.1.5'
+gem 'bootstrap-will_paginate', '1.0.0'
+
 
 # Use SCSS for stylesheets
 gem 'bootstrap-sass', '~> 3.3.7'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
 
     def index
-        @users = User.all
+        @users = User.paginate(page: params[:page],per_page: 5)
     end
-
-    
+ 
     def new
         @user = User.new
     end
@@ -41,8 +40,8 @@ class UsersController < ApplicationController
 
     def show 
         @user = User.find(params[:id])
+        @user_articles = @user.articles.paginate(page: params[:page],per_page: 1)
     end
-
 
 
     private 
